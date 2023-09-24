28bda365b808e3f1c6f35a7d2c958be54b9f5b66
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-09-24 10:20:38 +0000

    Styles index page

--- index.php
+++ index.php
@@ -32,13 +32,39 @@ require_once('assets/php/html_strings/nav_bar.php');
             color: #fff;
             text-align: center;
             line-height: 40px;
-        }  
+        }
+        
+        /* Custom CSS for the landing page */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+        }
+        .landing-container {
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        h1 {
+            font-size: 3em;
+            margin-bottom: 20px;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+        }
+        p {
+            font-size: 1.5em;
+            margin-bottom: 30px;
+        }
     </style>
 </head>
 <body>
 <?php echo $nav; ?>
-    <div class="container">
-        
+    <div class="landing-container">
+        <h1>Welcome to VideoVerse</h1>
+        <p>Your very own video hub</p>
+        <button type="button" href="explore.php" class="btn btn-primary btn-lg">Explore</button>
     </div>
 </body>
 </html>
